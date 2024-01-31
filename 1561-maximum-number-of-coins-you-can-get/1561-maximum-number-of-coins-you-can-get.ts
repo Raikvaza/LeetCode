915341bd294257bcf3f2a33b9f5ca82525d874eb
Akhmetzhan Seitkhan <114678986+Raikvaza@users.noreply.github.com>
2024-01-31 17:52:25 +0600

    Time: 1476 ms (6.25%), Space: 60 MB (12.50%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.ts
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.ts
@@ -1,6 +1,6 @@
 function maxCoins(piles: number[]): number {
   let res: number = 0
-    piles.sort((a, b) => b-a)
+    quickSort(piles)
     const len: number = piles.length
     
     for (let i=0; i<=len/3-1; i++) {
@@ -10,3 +10,28 @@ function maxCoins(piles: number[]): number {
     }
     return res
 };
+
+
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if (left < right) {
+        const pivotIndex = partition(arr, left, right);
+        quickSort(arr, left, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, right);
+    }
+    return arr;
+}
+
+function partition(arr, left, right) {
+    const pivot = arr[right];
+    let i = left - 1;
+    for (let j = left; j < right; j++) {
+        if (arr[j] > pivot) {
+            i++;
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+    }
+    [arr[i + 1], arr[right]] = [arr[right], arr[i + 1]]; 
+    i + 1
+    return i + 1;
+}
