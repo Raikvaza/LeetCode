026e90b53afb88830fc7565321123d7228d17177
Akhmetzhan Seitkhan <114678986+Raikvaza@users.noreply.github.com>
2024-02-02 12:39:38 +0600

    Time: 53 ms (64.88%), Space: 49.9 MB (13.07%) - LeetHub

---
+++ 2619-array-prototype-last/2619-array-prototype-last.ts
@@ -0,0 +1,17 @@
+declare global {
+    interface Array<T> {
+        last(): T | -1;
+    }
+}
+
+Array.prototype.last = function() {
+    
+    return this.length <=0 ? -1 : this[this.length-1]
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
+
+export {};
