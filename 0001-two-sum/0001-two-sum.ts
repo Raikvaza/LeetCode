c852cf1916027fb3152c44023cac9fecacb6e7c9
Akhmetzhan Seitkhan <114678986+Raikvaza@users.noreply.github.com>
2024-02-02 12:04:51 +0600

    Time: 54 ms (92.88%), Space: 52.9 MB (8.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,7 +1,12 @@
 function twoSum(nums: number[], target: number): number[] {
+    const temp: Map<number, number> = new Map()
+    
     for(let i=0; i<=nums.length-1; i++ ){
-        for(let j=i+1; j<=nums.length-1; j++){
-            if(nums[i]+nums[j]===target) return [i, j]
+        const diff = target - nums[i]
+        
+        if(temp.has(diff)){
+            return [temp.get(diff)!, i]            
         }
+        temp.set(nums[i], i)
     }
 };
